9f37de281947dabe9e084d032d20b651469830e2
AndySong <andylauszp@gmail.com>
2019-05-28 22:46:36 +1200

    add some json data which is useless

--- index.js
+++ index.js
@@ -1,6 +1,357 @@
+const data = {
+  addressMergeFields: [
+    {
+      html: 'customerModel:PostalAddressAddress0',
+      display: 'Customer Postal Careof'
+    },
+    {
+      html: 'customerModel:PostalAddressAddress1',
+      display: 'Customer Postal Address1'
+    },
+    {
+      html: 'customerModel:PostalAddressAddress2',
+      display: 'Customer Postal Address2'
+    },
+    {
+      html: 'customerModel:PostalAddressAddress3',
+      display: 'Customer Postal Address3'
+    },
+    {
+      html: 'customerModel:PostalAddressSuburb',
+      display: 'Customer Postal Suburb'
+    },
+    {
+      html: 'customerModel:PostalAddressCity',
+      display: 'Customer Postal City'
+    },
+    {
+      html: 'customerModel:PostalAddressState',
+      display: 'Customer Postal State'
+    },
+    {
+      html: 'customerModel:PostalAddressPostCode',
+      display: 'Customer Postal Postcode'
+    },
+    {
+      html: 'customerModel:PostalAddressCountry',
+      display: 'Customer Postal Country'
+    },
+    {
+      html: 'customerModel:PostalAddressDPID',
+      display: 'Customer Postal DPID'
+    },
+    {
+      html: 'customerModel:PhysicalAddressAddress0',
+      display: 'Customer Delivery Careof'
+    },
+    {
+      html: 'customerModel:PhysicalAddressAddress1',
+      display: 'Customer Delivery Address1'
+    },
+    {
+      html: 'customerModel:PhysicalAddressAddress2',
+      display: 'Customer Delivery Address2'
+    },
+    {
+      html: 'customerModel:PhysicalAddressAddress3',
+      display: 'Customer Delivery Address3'
+    },
+    {
+      html: 'customerModel:PhysicalAddressSuburb',
+      display: 'Customer Delivery Suburb'
+    },
+    {
+      html: 'customerModel:PhysicalAddressCity',
+      display: 'Customer Delivery City'
+    },
+    {
+      html: 'customerModel:PhysicalAddressState',
+      display: 'Customer Delivery State'
+    },
+    {
+      html: 'customerModel:PhysicalAddressPostCode',
+      display: 'Customer Delivery PostCode'
+    },
+    {
+      html: 'customerModel:PhysicalAddressCountry',
+      display: 'Customer Delivery Country'
+    },
+    {
+      html: 'customerModel:PhysicalAddressDPID',
+      display: 'Customer Delivery DPID'
+    }
+  ],
+  customerMergeFields: [
+    {
+      html: 'customerModel:CustomerGuid',
+      display: 'Globally Unique ID'
+    },
+    {
+      html: 'customerModel:Id',
+      display: 'Customer ID'
+    },
+    {
+      html: 'customerModel:ContactType',
+      display: 'Customer Contact Type'
+    },
+    {
+      html: 'customerModel:ContactTitle',
+      display: 'Customer Title'
+    },
+    {
+      html: 'customerModel:ContactInitials',
+      display: 'Customer Initials'
+    },
+    {
+      html: 'customerModel:ContactFirstName',
+      display: 'Customer First Name'
+    },
+    {
+      html: 'customerModel:ContactMiddleNames',
+      display: 'Customer Middle Names'
+    },
+    {
+      html: 'customerModel:ContactLastName',
+      display: 'Customer Last Name'
+    },
+    {
+      html: 'customerModel:ContactOrganisation',
+      display: 'Customer Organisation Name'
+    },
+    {
+      html: 'customerModel:ContactLetterTo',
+      display: 'Customer Letter'
+    },
+    {
+      html: 'customerModel:ContactSalutation',
+      display: 'Customer Salutation'
+    },
+    {
+      html: 'customerModel:RegionType',
+      display: 'Customer Region'
+    },
+    {
+      html: 'customerModel:HomePhone',
+      display: 'Customer Hone Phone'
+    },
+    {
+      html: 'customerModel:BusinessPhone',
+      display: 'Customer Business Phone'
+    },
+    {
+      html: 'customerModel:MobilePhone',
+      display: 'Customer Mobile'
+    },
+    {
+      html: 'customerModel:ContactFax',
+      display: 'Customer Fax'
+    },
+    {
+      html: 'customerModel:ContactSkype',
+      display: 'Customer Skype'
+    },
+    {
+      html: 'customerModel:ContactWebsite',
+      display: 'Customer Website'
+    },
+    {
+      html: 'customerModel:Email',
+      display: 'Customer Email'
+    },
+    {
+      html: 'customerModel:SourcedType',
+      display: 'Customer Sourced'
+    },
+    {
+      html: 'customerModel:GenderType',
+      display: 'Customer Gender'
+    },
+    {
+      html: 'customerModel:ContactBirthdate',
+      display: 'Customer Birth Date'
+    },
+    {
+      html: 'customerModel:ContactDeceasedDate',
+      display: 'Customer Deceased Date'
+    },
+    {
+      html: 'customerModel:ContactFaceBook',
+      display: 'Customer FaceBook'
+    },
+    {
+      html: 'customerModel:ContactLinkedIn',
+      display: 'Customer LinkedIn'
+    },
+    {
+      html: 'customerModel:ContactTwiter',
+      display: 'Customer Twitter'
+    },
+    {
+      html: 'customerModel:ContactMaidenName',
+      display: 'Customer Maiden Name'
+    },
+    {
+      html: 'customerModel:MaritalStatusType',
+      display: 'Customer Marital Status'
+    },
+    {
+      html: 'customerModel:EthnicityType',
+      display: 'Customer Ethnicity'
+    },
+    {
+      html: 'customerModel:BusinessTitleType',
+      display: 'Customer Business Title'
+    },
+    {
+      html: 'customerModel:OccupationType',
+      display: 'Customer Occupation'
+    },
+    {
+      html: 'customerModel:BranchType',
+      display: 'Customer Branch'
+    },
+    {
+      html: 'customerModel:ContactDepartment',
+      display: 'Customer Department'
+    },
+    {
+      html: 'customerModel:DD_BankType',
+      display: 'Customer Bank'
+    },
+    {
+      html: 'customerModel:DD_Branch',
+      display: 'Customer Branch'
+    },
+    {
+      html: 'customerModel:DD_AccountName',
+      display: 'Customer Account Name'
+    },
+    {
+      html: 'customerModel:DD_AccountNumber',
+      display: 'Customer Account No'
+    },
+    {
+      html: 'customerModel:CC_CardType',
+      display: 'Customer Card Type'
+    },
+    {
+      html: 'customerModel:CC_CardNumber',
+      display: 'Customer Card No'
+    },
+    {
+      html: 'customerModel:CC_ExpireDate',
+      display: 'Customer Expiry Date'
+    },
+    {
+      html: 'customerModel:CC_CardHolder',
+      display: 'Customer Card Holder'
+    }
+  ],
+  receiptMergeFields: [
+    {
+      html: 'receiptModel:Receipts_ReceiptId',
+      display: 'Receipt Number'
+    },
+    {
+      html: 'receiptModel:Receipts_BatchId',
+      display: 'Receipt Batch Number'
+    },
+    {
+      html: 'receiptModel:Receipts_Date',
+      display: 'Receipt Date'
+    },
+    {
+      html: 'receiptModel:Receipts_PaymentType',
+      display: 'Receipt Payment Type'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_CardType',
+      display: 'Card Type'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_CardHolder',
+      display: 'Card Holder'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_CardNumber',
+      display: 'Card Number'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_CardExpiry',
+      display: 'Card Expiry'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_CardToken',
+      display: 'Card Token'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_ProviderReference',
+      display: 'Provider Reference'
+    },
+    {
+      html: 'receiptModel:Receipts_CC_AuthorityNumber',
+      display: 'Authority Number'
+    },
+    {
+      html: 'receiptModel:Receipts_DD_BankType',
+      display: 'Bank Type'
+    },
+    {
+      html: 'receiptModel:Receipts_DD_BranchName',
+      display: 'Branch Name'
+    },
+    {
+      html: 'receiptModel:Receipts_Ch_DrawerName',
+      display: 'Drawer Name'
+    },
+    {
+      html: 'receiptModel:Receipts_Ch_ChequeNumber',
+      display: 'Cheque Number'
+    },
+    {
+      html: 'receiptModel:Receipts_Description',
+      display: 'Receipts Description'
+    }
+  ],
+  receiptProductsMergeFields: [
+    {
+      html: 'receiptProductModel:ReceiptsProducts_Product',
+      display: 'Product'
+    },
+    {
+      html: 'receiptProductModel:ReceiptsProducts_Campaign',
+      display: 'Campaign'
+    },
+    {
+      html: 'receiptProductModel:ReceiptProducts_Units',
+      display: 'Units'
+    },
+    {
+      html: 'receiptProductModel:ReceiptProducts_UnitTotal',
+      display: 'UnitTotal'
+    },
+    {
+      html: 'receiptProductModel:ReceiptProducts_Discount',
+      display: 'Discount'
+    },
+    {
+      html: 'receiptProductModel:ReceiptProducts_Tax',
+      display: 'Tax'
+    },
+    {
+      html: 'receiptProductModel:ReceiptProducts_Total',
+      display: 'Total'
+    }
+  ]
+}
 /* global unlayer*/
 window.onload = () => {
   unlayer.init({
     id: 'editor'
   })
+  unlayer.setMergeTags([
+    { name: 'First Name', value: '{{first_name}}' },
+    { name: 'Last Name', value: '{{last_name}}' }
+  ])
+  console.log(unlayer)
 }
